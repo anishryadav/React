c6e22a6a8053cc4db842213edd912374d0b66749
Anish Yadav <anishyadav60650@gmail.com>
2021-06-17 18:19:12 +0530

    components

--- confusion/src/App.jsx
+++ confusion/src/App.jsx
@@ -1,18 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import { Component } from 'react';
+import { DISHES } from './shared/dishes';
 
+class App extends Component{
 
-function App() {
-    return (
-        <div className="App" >
-        <Navbar dark color = "primary" >
-            <div className = "container" >
-                <NavbarBrand href="/" > Ristorante Con Fusion </NavbarBrand>
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    render() {
+        return (
+            <div className="App" >
+                <Navbar dark color="primary" >
+                    <div className="container" >
+                        <NavbarBrand href="/" > Ristorante Con Fusion </NavbarBrand>
+                    </div >
+                </Navbar>
+                <Menu dishes={this.state.dishes} />
             </div >
-        </Navbar> 
-        </div >
-    );
+        );
+    }
 }
 
 export default App;
